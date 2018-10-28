8c08b17163d3500b62a720801fb72937743fd8bf
wian777 <willi@andric.info>
2018-10-28 18:56:25 +0100

    ShowInfo ergänzt Changes in lbaseDotNetCustomControlsTC112_P2

---
+++ src/ShowInfo/ShowInfo/ShowInfo.cs
@@ -0,0 +1,31 @@
+﻿using Tricentis.Automation.AutomationInstructions.Dynamic.Values;
+using Tricentis.Automation.AutomationInstructions.TestActions;
+using Tricentis.Automation.Creation;
+using Tricentis.Automation.Engines;
+using Tricentis.Automation.Engines.SpecialExecutionTasks;
+using Tricentis.Automation.Engines.SpecialExecutionTasks.Attributes;
+
+namespace Toolbox
+{
+    [SpecialExecutionTaskName("CustomedShowInfo")]
+    public class ShowInfo : SpecialExecutionTask
+    {
+        private const string c_ShowInfo = "ShowInfo";
+        
+        public ShowInfo(Validator validator) : base(validator)
+        {
+        }
+
+        public override ActionResult Execute(ISpecialExecutionTaskTestAction testAction)
+        {
+            IInputValue showinfo = testAction.GetParameterAsInputValue(c_ShowInfo, false);
+
+            /*
+            if (showinfo == null || string.IsNullOrEmpty(showinfo.Value))
+                throw new ArgumentException(string.Format("Mandatory parameter '{0}' not set.", c_ShowInfo));
+            */
+
+            return new PassedActionResult("Info: " + showinfo.Value );
+        }
+    }
+}
